c924290c9e1dae9d144c52689907d1209f92cf77
Nikolaj Schumacher <git@nschum.de>
2013-05-28 12:18:21 +0200

    Add tests to iOS project.

---
+++ FontAwesomeIconFactoryTests/FontAwesomeIconFactoryTests.h
@@ -0,0 +1,9 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wobjc-interface-ivars"
+#pragma clang diagnostic ignored "-Wgnu"
+#import <SenTestingKit/SenTestingKit.h>
+#pragma clang diagnostic pop
+
+@interface FontAwesomeIconFactoryTests : SenTestCase
+
+@end
---
+++ FontAwesomeIconFactoryTests/FontAwesomeIconFactoryTests.m
@@ -0,0 +1,18 @@
+#import "FontAwesomeIconFactoryTests.h"
+#import "AppDelegate.h"
+
+@implementation FontAwesomeIconFactoryTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testExample {
+    STAssertNotNil([AppDelegate new], nil);
+}
+
+@end
