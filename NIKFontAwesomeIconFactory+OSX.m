96778ebae60871c48189a13107f367d38d766257
Nikolaj Schumacher <git@nschum.de>
2013-06-14 23:39:53 +0200

    Use font height instead of ascent to scale font.

--- NIKFontAwesomeIconFactory+OSX.m
+++ NIKFontAwesomeIconFactory+OSX.m
@@ -6,21 +6,21 @@
 
 + (instancetype)bevelButtonIconFactory {
     NIKFontAwesomeIconFactory *factory = [NIKFontAwesomeIconFactory new];
-    factory.size = 14.0;
+    factory.size = 16.0;
     factory.edgeInsets = NSEdgeInsetsMake(0, 2.0, 0, 2.0);
     return factory;
 }
 
 + (instancetype)pushButtonIconFactory {
     NIKFontAwesomeIconFactory *factory = [NIKFontAwesomeIconFactory new];
-    factory.size = 14.0;
-    factory.edgeInsets = NSEdgeInsetsMake(2.0, 10.0, 0, 0.0);
+    factory.size = 12.0;
+    factory.edgeInsets = NSEdgeInsetsMake(0.0, 10.0, 2.0, 0.0);
     return factory;
 }
 
 + (instancetype)toolbarItemIconFactory {
     NIKFontAwesomeIconFactory *factory = [NIKFontAwesomeIconFactory new];
-    factory.size = 32.0;
+    factory.size = 38.0;
     factory.square = YES;
     return factory;
 }
